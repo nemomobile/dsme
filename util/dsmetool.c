46d2af7ed886318973ba064ef91a2aa6c92dbcb3
Semi Malinen <semi.malinen@nokia.com>
2009-05-13 12:53:20 +0000

    minor edit: indent
    git-svn-id: https://projects.maemo.org/svn/dsm/dsme/trunk@12212 db81c003-b5ea-0310-aa99-ed7f6e1db739

--- util/dsmetool.c
+++ util/dsmetool.c
@@ -47,8 +47,8 @@
 
 void usage(const char* name);
 static int send_process_start_request(const char*       command,
-				      process_actions_t action,
-				      int               maxcount,
+                                      process_actions_t action,
+                                      int               maxcount,
                                       int               maxperiod,
                                       uid_t             uid,
                                       gid_t             gid,
@@ -60,8 +60,8 @@ static dsmesock_connection_t* conn;
 
 void usage(const char* name)
 {
-	printf("USAGE: %s <options>\n", name);
-	printf(
+    printf("USAGE: %s <options>\n", name);
+    printf(
 "Note that the <cmd> should include absolute path.\n"
 "  -r --start-reset=<cmd>          Start a process\n"
 "                                   (on process exit, do SW reset)\n"
@@ -102,67 +102,67 @@ void usage(const char* name)
 
 static void connect_to_dsme(void)
 {
-	conn = dsmesock_connect();
-	if (conn == 0) {
-		perror("dsmesock_connect");
-		exit(EXIT_FAILURE);
-	}
+    conn = dsmesock_connect();
+    if (conn == 0) {
+        perror("dsmesock_connect");
+        exit(EXIT_FAILURE);
+    }
 }
 
 static int get_version(void)
 {
-	DSM_MSGTYPE_GET_VERSION   req_msg =
+    DSM_MSGTYPE_GET_VERSION   req_msg =
           DSME_MSG_INIT(DSM_MSGTYPE_GET_VERSION);
-        void*                     p;
-	DSM_MSGTYPE_DSME_VERSION* retmsg = NULL;
-	fd_set                    rfds;
-	int                       ret = -1;
+    void*                     p;
+    DSM_MSGTYPE_DSME_VERSION* retmsg = NULL;
+    fd_set                    rfds;
+    int                       ret = -1;
 
-	printf("dsmetool version: %s\n", STRINGIFY(PRG_VERSION));
+    printf("dsmetool version: %s\n", STRINGIFY(PRG_VERSION));
 
-        connect_to_dsme();
-	
-	dsmesock_send(conn, &req_msg);
+    connect_to_dsme();
 
-	while (1) {
-		FD_ZERO(&rfds);
-		FD_SET(conn->fd, &rfds);
-		struct timeval tv;
+    dsmesock_send(conn, &req_msg);
 
-		tv.tv_sec = 5;
-		tv.tv_usec = 0;
-		
-		ret = select(conn->fd+1, &rfds, NULL, NULL, &tv);
-		if (ret == -1) {
-			fprintf(stderr, "Error in select()\n");
-			return -1;
-		}
-		if (ret == 0) {
-			fprintf(stderr, "Timeout when getting the DSME version\n");
-			dsmesock_close(conn);
-			return -1;
-		}
+    while (1) {
+        FD_ZERO(&rfds);
+        FD_SET(conn->fd, &rfds);
+        struct timeval tv;
 
-		p = dsmesock_receive(conn);
-		
-                if ((retmsg = DSMEMSG_CAST(DSM_MSGTYPE_DSME_VERSION, p)) == 0) {
-			fprintf(stderr, "Received invalid message\n");
-			free(p);
-			continue;
-                }
-		break;
-	}
+        tv.tv_sec = 5;
+        tv.tv_usec = 0;
 
-        char* version = (char*)DSMEMSG_EXTRA(retmsg);
-        if (version != 0) {
-          version[DSMEMSG_EXTRA_SIZE(retmsg) - 1] = '\0';
-          printf("DSME version: %s\n", version);
-	}
+        ret = select(conn->fd+1, &rfds, NULL, NULL, &tv);
+        if (ret == -1) {
+            fprintf(stderr, "Error in select()\n");
+            return -1;
+        }
+        if (ret == 0) {
+            fprintf(stderr, "Timeout when getting the DSME version\n");
+            dsmesock_close(conn);
+            return -1;
+        }
 
-        free(p);
-	dsmesock_close(conn);
+        p = dsmesock_receive(conn);
 
-	return 0;
+        if ((retmsg = DSMEMSG_CAST(DSM_MSGTYPE_DSME_VERSION, p)) == 0) {
+            fprintf(stderr, "Received invalid message\n");
+            free(p);
+            continue;
+        }
+        break;
+    }
+
+    char* version = (char*)DSMEMSG_EXTRA(retmsg);
+    if (version != 0) {
+        version[DSMEMSG_EXTRA_SIZE(retmsg) - 1] = '\0';
+        printf("DSME version: %s\n", version);
+    }
+
+    free(p);
+    dsmesock_close(conn);
+
+    return 0;
 }
 
 static int send_process_start_request(const char*       command,
@@ -173,47 +173,47 @@ static int send_process_start_request(const char*       command,
                                       gid_t             gid,
                                       int               nice)
 {
-  DSM_MSGTYPE_PROCESS_START        msg =
-    DSME_MSG_INIT(DSM_MSGTYPE_PROCESS_START);
-  DSM_MSGTYPE_PROCESS_STARTSTATUS* retmsg;
-  fd_set rfds;
-  int ret;
+    DSM_MSGTYPE_PROCESS_START        msg =
+      DSME_MSG_INIT(DSM_MSGTYPE_PROCESS_START);
+    DSM_MSGTYPE_PROCESS_STARTSTATUS* retmsg;
+    fd_set rfds;
+    int    ret;
 
-  msg.action         = action;
-  msg.restart_limit  = maxcount;
-  msg.restart_period = maxperiod;
-  msg.uid            = uid;
-  msg.gid            = gid;
-  msg.nice           = nice;
-  dsmesock_send_with_extra(conn, &msg, strlen(command) + 1, command);
+    msg.action         = action;
+    msg.restart_limit  = maxcount;
+    msg.restart_period = maxperiod;
+    msg.uid            = uid;
+    msg.gid            = gid;
+    msg.nice           = nice;
+    dsmesock_send_with_extra(conn, &msg, strlen(command) + 1, command);
 
-  while (1) {
-      FD_ZERO(&rfds);
-      FD_SET(conn->fd, &rfds);
+    while (1) {
+        FD_ZERO(&rfds);
+        FD_SET(conn->fd, &rfds);
 
-      ret = select(conn->fd+1, &rfds, NULL, NULL, NULL);
-      if (ret == -1) {
-          printf("Error in select()\n");
-          return -1;
-      }
-
-      retmsg = (DSM_MSGTYPE_PROCESS_STARTSTATUS*)dsmesock_receive(conn);
-
-      if (DSMEMSG_CAST(DSM_MSGTYPE_PROCESS_STARTSTATUS, retmsg) == 0)
-        {
-          printf("Received invalid message (type: %i)\n",
-                 dsmemsg_id((dsmemsg_generic_t*)retmsg));
-          free(retmsg);
-          continue;
+        ret = select(conn->fd+1, &rfds, NULL, NULL, NULL);
+        if (ret == -1) {
+            printf("Error in select()\n");
+            return -1;
         }
 
-      /* printf("PID=%d, startval=%d\n", retmsg->pid, retmsg->return_value); */
+        retmsg = (DSM_MSGTYPE_PROCESS_STARTSTATUS*)dsmesock_receive(conn);
 
-      ret = retmsg->status;
-      free(retmsg);
+        if (DSMEMSG_CAST(DSM_MSGTYPE_PROCESS_STARTSTATUS, retmsg) == 0)
+        {
+            printf("Received invalid message (type: %i)\n",
+                   dsmemsg_id((dsmemsg_generic_t*)retmsg));
+            free(retmsg);
+            continue;
+        }
 
-      return ret;
-  }
+        /* printf("PID=%d, startval=%d\n", retmsg->pid, retmsg->return_value); */
+
+        ret = retmsg->status;
+        free(retmsg);
+
+        return ret;
+    }
 }
 
 static int send_process_stop_request(const char* command, int signal)
@@ -250,179 +250,177 @@ static int send_dbus_service_end_request()
 
 int main(int argc, char* argv[])
 {
+    const char* program_name  = argv[0];
+    int         next_option;
+    int         retval        =  0;
+    int         maxcount      = 10;
+    int         countperiod   = 60;
+    int         signum        = 15;
+    uid_t       uid           = getuid();
+    gid_t       gid           = getgid();
+    int         group_set     = 0;
+    const char* username      = 0;
+    const char* group         = 0;
+    int         nice          = 0;
+    enum { NONE, START, STOP } action = NONE;
+    const char* program       = "";
+    process_actions_t policy  = ONCE;
+    const char* short_options = "n:hr:f:t:o:c:T:k:S:u:g:U:G:dev";
+    const struct option long_options[] = {
+        {"help",               0, NULL, 'h'},
+        {"start-reset",        1, NULL, 'r'},
+        {"start-restart",      1, NULL, 't'},
+        {"start-restart-fail", 1, NULL, 'f'},
+        {"start-once",         1, NULL, 'o'},
+        {"max-count",          1, NULL, 'c'},
+        {"count-time",         1, NULL, 'T'},
+        {"stop",               1, NULL, 'k'},
+        {"signal",             1, NULL, 'S'},
+        {"uid",                1, NULL, 'u'},
+        {"gid",                1, NULL, 'g'},
+        {"user",               1, NULL, 'U'},
+        {"group",              1, NULL, 'G'},
+        {"nice",               1, NULL, 'n'},
+        {"start-dbus",         0, NULL, 'd'},
+        {"stop-dbus",          0, NULL, 'e'},
+        {"version",            0, NULL, 'v'},
+        {0, 0, 0, 0}
+    };
+
+    do {
+        next_option =
+            getopt_long(argc, argv, short_options, long_options, NULL);
+        switch (next_option) {
+            case 'k':
+                program = optarg;
+                action = STOP;
+                break;
+            case 'S':
+                signum = atoi(optarg);
+                break;
+            case 'u':
+                uid = atoi(optarg);
+                break;
+            case 'U':
+                username = optarg;
+                break;
+            case 'g':
+                gid = atoi(optarg);
+                group_set = 1;
+                break;
+            case 'G':
+                group = optarg;
+                group_set = 1;
+                break;
+            case 'n':
+                nice = atoi(optarg);
+                break;
+            case 'c':
+                maxcount = atoi(optarg);
+                break;
+            case 'T':
+                countperiod = atoi(optarg);
+                break;
+            case 'r':
+                program = optarg;
+                policy = RESET;
+                action = START;
+                break;
+            case 't':
+                program = optarg;
+                policy = RESPAWN;
+                action = START;
+                break;
+            case 'f':
+                program = optarg;
+                policy = RESPAWN_FAIL;
+                action = START;
+                break;
+            case 'o':
+                program = optarg;
+                policy = ONCE;
+                action = START;
+                break;
+            case 'd':
+                return send_dbus_service_start_request();
+                break;
+            case 'e':
+                return send_dbus_service_end_request();
+                break;
+            case 'v':
+                return get_version();
+                break;
+            case 'h':
+                usage(program_name);
+                return EXIT_SUCCESS;
+                break;
+            case '?':
+                usage(program_name);
+                return EXIT_FAILURE;
+                break;
+        }
+    } while (next_option != -1);
+
+    /* check if unknown parameters or no parameters at all were given */
+    if (argc == 1 || optind < argc) {
+        usage(program_name);
+        return EXIT_FAILURE;
+    }
+
+    if (username != 0) {
+        struct passwd *pw_entry = getpwnam(username);
+
+        if (uid != getuid())
+            printf("warning, username overrides specified uid\n");
+
+        if (!pw_entry) {
+            printf("Can't get a UID for username: %s\n", username); 
+            return EXIT_FAILURE;
+        }
+        uid = pw_entry->pw_uid;
+    }
 
 
-	const char* program_name  = argv[0];
-	int         next_option;
-	int         retval        =  0;
-	int         maxcount      = 10;
-	int         countperiod   = 60;
-	int         signum        = 15;
-	uid_t       uid           = getuid();
-	gid_t       gid           = getgid();
-	int         group_set     = 0;
-	const char* username      = 0;
-	const char* group         = 0;
-	int         nice          = 0;
-	enum { NONE, START, STOP } action = NONE;
-	const char* program       = "";
-	process_actions_t policy  = ONCE;
-	const char* short_options = "n:hr:f:t:o:c:T:k:S:u:g:U:G:dev";
-	const struct option long_options[] = {
-		{"help",               0, NULL, 'h'},
-		{"start-reset",        1, NULL, 'r'},
-		{"start-restart",      1, NULL, 't'},
-		{"start-restart-fail", 1, NULL, 'f'},
-		{"start-once",         1, NULL, 'o'},
-		{"max-count",          1, NULL, 'c'},
-		{"count-time",         1, NULL, 'T'},
-		{"stop",               1, NULL, 'k'},
-		{"signal",             1, NULL, 'S'},
-		{"uid",                1, NULL, 'u'},
-		{"gid",                1, NULL, 'g'},
-		{"user",               1, NULL, 'U'},
-		{"group",              1, NULL, 'G'},
-		{"nice",               1, NULL, 'n'},
-                {"start-dbus",         0, NULL, 'd'},
-                {"stop-dbus",          0, NULL, 'e'},
-		{"version",            0, NULL, 'v'},
-		{0, 0, 0, 0}
-	};
+    if (group != 0) {
+        struct group* gr_entry = getgrnam(group);
 
-	do {
-		next_option =
-                  getopt_long(argc, argv, short_options, long_options, NULL);
-		switch (next_option) {
-		case 'k':
-			program = optarg;
-			action = STOP;
-			break;
-		case 'S':
-			signum = atoi(optarg);
-			break;
-		case 'u':
-			uid = atoi(optarg);
-			break;
-		case 'U':
-			username = optarg;
-			break;
-		case 'g':
-			gid = atoi(optarg);
-			group_set = 1;
-			break;
-		case 'G':
-			group = optarg;
-			group_set = 1;
-			break;
-		case 'n':
-			nice = atoi(optarg);
-			break;
-		case 'c':
-			maxcount = atoi(optarg);
-			break;
-		case 'T':
-			countperiod = atoi(optarg);
-			break;
-		case 'r':
-			program = optarg;
-			policy = RESET;
-			action = START;
-			break;
-		case 't':
-			program = optarg;
-			policy = RESPAWN;
-			action = START;
-			break;
-		case 'f':
-			program = optarg;
-			policy = RESPAWN_FAIL;
-			action = START;
-			break;
-		case 'o':
-			program = optarg;
-			policy = ONCE;
-			action = START;
-			break;
-                case 'd':
-                        return send_dbus_service_start_request();
-                        break;
-                case 'e':
-                        return send_dbus_service_end_request();
-                        break;
-		case 'v':
-			return get_version();
-			break;
-		case 'h':
-			usage(program_name);
-			return EXIT_SUCCESS;
-			break;
-		case '?':
-			usage(program_name);
-			return EXIT_FAILURE;
-			break;
-		}
-	} while (next_option != -1);
-	
-	/* check if unknown parameters or no parameters at all were given */
-	if (argc == 1 || optind < argc) {
-		usage(program_name);
-		return EXIT_FAILURE;
-	}
+        if (gid != getgid())
+            printf("warning, group overrides specified gid\n");
 
-	if (username != 0) {
-		struct passwd *pw_entry = getpwnam(username);
-		
-		if (uid != getuid())
-			printf("warning, username overrides specified uid\n");
+        if (!gr_entry) {
+            printf("Can't get a GID for groupname: %s\n", group);
+            return EXIT_FAILURE;
+        }
+        gid = gr_entry->gr_gid;
+    }
 
-		if (!pw_entry) {
-			printf("Can't get a UID for username: %s\n", username); 
-			return EXIT_FAILURE;
-		}
-		uid = pw_entry->pw_uid;
-	}
+    if (uid != getuid() && !group_set) {
+        struct passwd *pw_entry = getpwuid(uid);
+        if (!pw_entry) {
+            printf("Can't get pwentry for UID: %d\n", uid);
+            return EXIT_FAILURE;
+        }
+        if (pw_entry->pw_gid)
+            gid = pw_entry->pw_gid;
+        else
+            printf("Default group not found for UID: %d. Using current one.\n", uid);
+    }
 
+    connect_to_dsme();
 
-	if (group != 0) {
-		struct group* gr_entry = getgrnam(group);
+    if (action == START) {
+        retval = send_process_start_request(program,
+                                            policy,
+                                            maxcount,
+                                            countperiod,
+                                            uid,
+                                            gid,
+                                            nice);
+    } else if (action == STOP) {
+        send_process_stop_request(program, signum);
+    }
 
-		if (gid != getgid())
-			printf("warning, group overrides specified gid\n");
-		
-		if (!gr_entry) {
-			printf("Can't get a GID for groupname: %s\n", group);
-			return EXIT_FAILURE;
-		}
-		gid = gr_entry->gr_gid;
-	}
+    dsmesock_close(conn);
 
-	if (uid != getuid() && !group_set) {
-		struct passwd *pw_entry = getpwuid(uid);
-		if (!pw_entry) {
-			printf("Can't get pwentry for UID: %d\n", uid);
-			return EXIT_FAILURE;
-		}
-		if (pw_entry->pw_gid)
-			gid = pw_entry->pw_gid;
-		else
-			printf("Default group not found for UID: %d. Using current one.\n", uid);
-	}
-
-        connect_to_dsme();
-
-	if (action == START) {
-		retval = send_process_start_request(program,
-                                                    policy,
-						    maxcount,
-                                                    countperiod,
-                                                    uid,
-                                                    gid,
-                                                    nice);
-	} else if (action == STOP) {
-		send_process_stop_request(program, signum);
-	}
-
-	dsmesock_close(conn);
-
-	return retval;
+    return retval;
 }
