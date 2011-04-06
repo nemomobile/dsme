1afaad27fa9b414d89d83c3ea2455adbb57a0b37
Matias Muhonen <ext-matias.muhonen@nokia.com>
2011-04-06 12:05:13 +0300

    Adding the D-Bus interface for diskmonitor

--- modules/diskmonitor.c
+++ modules/diskmonitor.c
@@ -45,6 +45,50 @@
 
 static bool init_done_received = false;
 
+static bool dbus_methods_bound = false;
+static bool dbus_signals_bound = false;
+
+/* ========================================================================= *
+ * D-Bus Query API
+ * ========================================================================= */
+
+static const char diskmonitor_service[]               = "com.nokia.diskmonitor";
+static const char diskmonitor_req_interface[]         = "com.nokia.diskmonitor.request";
+static const char diskmonitor_sig_interface[]         = "com.nokia.diskmonitor.signal";
+static const char diskmonitor_req_path[]              = "/com/nokia/diskmonitor/request";
+static const char diskmonitor_sig_path[]              = "/com/nokia/diskmonitor/signal";
+
+static const char diskmonitor_req_check[]             = "req_check";
+static const char diskmonitor_disk_space_change_ind[] = "disk_space_change_ind";
+
+static void req_check(const DsmeDbusMessage* request, DsmeDbusMessage** reply)
+{
+    check_disk_space_usage();
+}
+
+static const dsme_dbus_binding_t methods[] =
+{
+    { req_check, diskmonitor_req_check },
+    { 0, 0 }
+};
+
+static void init_done_ind(const DsmeDbusMessage* ind)
+{
+    dsme_log(LOG_DEBUG, "base_boot_done");
+
+    init_done_received = true;
+}
+
+static const dsme_dbus_signal_binding_t signals[] =
+{
+    { init_done_ind, "com.nokia.startup.signal", "base_boot_done" },
+    { 0, 0 }
+};
+
+/* ========================================================================= *
+ * Helpers
+ * ========================================================================= */
+
 static void schedule_next_wakeup(void)
 {
     /* Don't cause too frequent wakeups for checking the disks;
@@ -65,6 +109,10 @@ static bool init_done(void)
     return init_done_received;
 }
 
+/* ========================================================================= *
+ * Internal DSME event handling
+ * ========================================================================= */
+
 DSME_HANDLER(DSM_MSGTYPE_WAKEUP, client, msg)
 {
     if (init_done()) {
@@ -74,39 +122,47 @@ DSME_HANDLER(DSM_MSGTYPE_WAKEUP, client, msg)
     schedule_next_wakeup();
 }
 
-
-static void init_done_ind(const DsmeDbusMessage* ind)
-{
-    dsme_log(LOG_DEBUG, "base_boot_done");
-    init_done_received = true;
-}
-
-static bool bound = false;
-
-static const dsme_dbus_signal_binding_t signals[] = {
-    { init_done_ind, "com.nokia.startup.signal", "base_boot_done" },
-    { 0, 0 }
-};
-
 DSME_HANDLER(DSM_MSGTYPE_DBUS_CONNECT, client, msg)
 {
-  dsme_log(LOG_DEBUG, "diskmonitor: DBUS_CONNECT");
-  dsme_dbus_bind_signals(&bound, signals);
+    dsme_log(LOG_DEBUG, "diskmonitor: DBUS_CONNECT");
+
+    dsme_dbus_bind_methods(&dbus_methods_bound, methods, diskmonitor_service, diskmonitor_req_interface);
+    dsme_dbus_bind_signals(&dbus_signals_bound, signals);
 }
 
 DSME_HANDLER(DSM_MSGTYPE_DBUS_DISCONNECT, client, msg)
 {
-  dsme_log(LOG_DEBUG, "diskmonitor: DBUS_DISCONNECT");
-  dsme_dbus_unbind_signals(&bound, signals);
+   dsme_log(LOG_DEBUG, "diskmonitor: DBUS_DISCONNECT");
+
+   dsme_dbus_unbind_methods(&dbus_methods_bound, methods, diskmonitor_service, diskmonitor_req_interface);
+   dsme_dbus_unbind_signals(&dbus_signals_bound, signals);
+}
+
+DSME_HANDLER(DSM_MSGTYPE_DISK_SPACE, conn, msg)
+{
+    const char* mount_path = DSMEMSG_EXTRA(msg);
+    DsmeDbusMessage* sig =
+        dsme_dbus_signal_new(diskmonitor_sig_path,
+                             diskmonitor_sig_interface,
+                             diskmonitor_disk_space_change_ind);
+
+    dsme_dbus_message_append_int(sig, msg->blocks_percent_used);
+    dsme_dbus_message_append_string(sig, mount_path);
+    dsme_dbus_signal_emit(sig);
 }
 
 module_fn_info_t message_handlers[] =
 {
-  DSME_HANDLER_BINDING(DSM_MSGTYPE_WAKEUP),
-  DSME_HANDLER_BINDING(DSM_MSGTYPE_DBUS_CONNECT),
-  DSME_HANDLER_BINDING(DSM_MSGTYPE_DBUS_DISCONNECT),
-  { 0 }
+    DSME_HANDLER_BINDING(DSM_MSGTYPE_WAKEUP),
+    DSME_HANDLER_BINDING(DSM_MSGTYPE_DBUS_CONNECT),
+    DSME_HANDLER_BINDING(DSM_MSGTYPE_DBUS_DISCONNECT),
+    DSME_HANDLER_BINDING(DSM_MSGTYPE_DISK_SPACE),
+    { 0 }
 };
+
+/* ========================================================================= *
+ * Plugin init and fini
+ * ========================================================================= */
 
 void module_init(module_t* module)
 {
