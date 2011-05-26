1ccf90022e3964557d90ca345340f79bcda557bd
Matias Muhonen <ext-matias.muhonen@nokia.com>
2011-05-26 12:26:35 +0300

    Fix a race between waitfordsme and dsmesock/accept_client

--- util/waitfordsme.c
+++ util/waitfordsme.c
@@ -4,9 +4,10 @@
    This program blocks until the DSME socket is ready.
    Timeout is also defined.
    <p>
-   Copyright (C) 2004-2010 Nokia Corporation.
+   Copyright (C) 2004-2011 Nokia Corporation.
 
    @author Ismo Laitinen
+   @author Matias Muhonen <ext-matias.muhonen@nokia.com>
 
    This file is part of Dsme.
 
@@ -24,7 +25,9 @@
 */
 
 #include "dsme/protocol.h"
+#include "dsme/state.h"
 
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -35,11 +38,21 @@
 
 #define DSME_START_TIMEOUT 5
 
+static bool send_to_dsme(dsmesock_connection_t* conn, const void* msg)
+{
+	bool success = true;
+	if (dsmesock_send(conn, msg) == -1) {
+		success = false;
+	}
+	return success;
+}
+
 int main(int argc, char* argv[]) {
 	
 	struct timeval start;
 	struct timeval now;
 	dsmesock_connection_t *conn;
+	DSM_MSGTYPE_GET_VERSION req_msg = DSME_MSG_INIT(DSM_MSGTYPE_GET_VERSION);
 
 	gettimeofday(&start, NULL);
 
@@ -49,9 +62,17 @@ int main(int argc, char* argv[]) {
 	while (1) {
 		conn = dsmesock_connect();
 		if (conn > 0) {
-			dsmesock_close(conn);
-			printf("%s: OK: DSME socket exists\n", argv[0]);
-			return EXIT_SUCCESS;
+			/* Give DSME time to accept the client socket before closing
+			   the socket connection */
+			if (!send_to_dsme(conn, &req_msg)) {
+				dsmesock_close(conn);
+				printf("%s: ERROR: send to DSME socket failed\n", argv[0]);
+				return EXIT_FAILURE;
+			} else {
+				dsmesock_close(conn);
+				printf("%s: OK: send to DSME socket succeeded\n", argv[0]);
+				return EXIT_SUCCESS;
+			}
 		}
 		
 		gettimeofday(&now, NULL);
