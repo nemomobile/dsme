91bd809814f517674b99a65e8e4952337a9a77d5
Pekka Lundstrom <pekka.lundstrom@jollamobile.com>
2013-09-24 12:47:34 +0300

    [thermal] Added thermal sensors for battery and core
    Signed-off-by: Pekka Lundstrom <pekka.lundstrom@jollamobile.com>

---
+++ modules/thermalobject_hw.c
@@ -0,0 +1,108 @@
+/**
+   @file thermalobject_hw.c
+
+   This file implements a thermal object for tracking HW temperatures.
+   Battery and SOC core temperatures
+   <p>
+   Copyright (C) 2013 Jolla ltd
+
+   @author Pekka Lundstrom <pekka.lundstrom@jolla.com>
+
+   This file is part of Dsme.
+
+   Dsme is free software; you can redistribute it and/or modify
+   it under the terms of the GNU Lesser General Public License
+   version 2.1 as published by the Free Software Foundation.
+
+   Dsme is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Lesser General Public License for more details.
+
+   You should have received a copy of the GNU Lesser General Public
+   License along with Dsme.  If not, see <http://www.gnu.org/licenses/>.
+*/
+#define _GNU_SOURCE
+
+#include "thermalmanager.h"
+#include "thermalsensor_hw.h"
+
+#include "dsme/modules.h"
+#include "dsme/logging.h"
+
+static bool core_registered = false;
+static bool battery_registered = false;
+
+
+/* These are default values that can be overridden by
+   HW adaptation file /etc/dsme/temp_hw_core
+*/
+static thermal_object_configuration_t core_thermal_conf = {
+  "hw_core",
+  {
+      /* (min, max], [mintime, maxtime] */
+      {   -99,  70,       110,     120 }, /* NORMAL  */
+      {    68,  90,        55,      60 }, /* WARNING */
+      {    88, 115,         5,      10 }, /* ALERT   */
+      {   115, 999,         5,      10 }  /* FATAL   */
+  },
+  dsme_hw_get_core_temperature
+};
+
+static thermal_object_t core_thermal_object = {
+  &core_thermal_conf,
+  THERMAL_STATUS_NORMAL,
+  0
+};
+
+/* These are default values that can be overridden by
+   HW adaptation file /etc/dsme/temp_hw_battery
+*/
+static thermal_object_configuration_t battery_thermal_conf = {
+  "hw_battery",
+  {
+      /* (min, max], [mintime, maxtime] */
+      {   -99,  57,       110,     120 }, /* NORMAL  */
+      {    56,  60,        55,      60 }, /* WARNING */
+      {    59,  65,         5,      10 }, /* ALERT   */
+      {    65,  99,         5,      10 }, /* FATAL   */
+  },
+  dsme_hw_get_battery_temperature
+};
+
+static thermal_object_t battery_thermal_object = {
+  &battery_thermal_conf,
+  THERMAL_STATUS_NORMAL,
+  0
+};
+
+
+void module_init(module_t* handle)
+{
+    dsme_log(LOG_DEBUG, "thermalobject_hw.so loaded");
+    if (dsme_hw_supports_core_temp()) {
+        dsme_register_thermal_object(&core_thermal_object);
+        core_registered = true;
+    } else {
+        dsme_log(LOG_NOTICE, "thermalobject_hw_core: No HW support, going to sleep only mode");
+    }
+    if (dsme_hw_supports_battery_temp()) {
+        dsme_register_thermal_object(&battery_thermal_object);
+        battery_registered = true;
+    } else {
+        dsme_log(LOG_NOTICE, "thermalobject_hw_battery: No HW support, going to sleep only mode");
+    }
+}
+
+void module_fini(void)
+{
+    if (core_registered) {
+        dsme_unregister_thermal_object(&core_thermal_object);
+        core_registered = false;
+    }
+    if (battery_registered) {
+        dsme_unregister_thermal_object(&battery_thermal_object);
+        battery_registered = false;
+    }
+    dsme_log(LOG_DEBUG, "thermalobject_hw.so unloaded");
+}
---
+++ modules/thermalsensor_hw.c
@@ -0,0 +1,138 @@
+/**
+   @file thermalsensor_hw.c
+
+   This module provides HW temperature readings.
+   <p>
+   Copyright (C) 2013 Jolla ltd
+
+   @author Pekka Lundstrom <pekka.lundstrom@jolla.com>
+
+   This file is part of Dsme.
+
+   Dsme is free software; you can redistribute it and/or modify
+   it under the terms of the GNU Lesser General Public License
+   version 2.1 as published by the Free Software Foundation.
+
+   Dsme is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Lesser General Public License for more details.
+
+   You should have received a copy of the GNU Lesser General Public
+   License along with Dsme.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#define _GNU_SOURCE
+#include "thermalmanager.h"
+#include "thermalsensor_hw.h"
+#include "dsme/logging.h"
+
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+
+static bool read_temperature(const char *path, int *temperature)
+{
+    FILE* fd; 
+    bool  ret = false;
+    // dsme_log(LOG_DEBUG, "thermal: %s", __FUNCTION__);
+
+    fd = fopen(path, "r");
+    if (fd) {
+        if (fscanf(fd, "%d", temperature) == 1) {
+            // dsme_log(LOG_DEBUG, "thermal: %s = %d C", path, *temperature);
+            ret = true;
+        }
+        fclose(fd);
+    } else {
+        dsme_log(LOG_DEBUG, "thermal: read of %s FAILED", path);
+    }
+    return ret;
+}
+
+/* Get temp from sensor 9 */
+/* TODO: This should be changed to use statefs once we have it */
+
+#define CORE_SENSOR_MODE "/sys/devices/virtual/thermal/thermal_zone9/mode"
+#define CORE_SENSOR_TEMP "/sys/devices/virtual/thermal/thermal_zone9/temp"
+
+extern bool dsme_hw_get_core_temperature(thermal_object_t*         thermal_object,
+                                         temperature_handler_fn_t* callback)
+{
+    int temperature;
+
+    // dsme_log(LOG_DEBUG, "thermal: %s", __FUNCTION__);
+
+    if (read_temperature(CORE_SENSOR_TEMP, &temperature)) {
+        if (callback) 
+            callback(thermal_object, temperature);
+        return true;
+    } else {
+        return false;
+    }
+}
+
+static bool enable_hw_core_temp_sensor(void)
+{
+    FILE* fd; 
+    bool ret = false;
+
+    // dsme_log(LOG_DEBUG, "thermal: %s", __FUNCTION__);
+
+    fd = fopen(CORE_SENSOR_MODE, "w");
+    if (fd) {
+        if (fprintf(fd, "enabled") > 0) {
+            ret = true;
+        }
+        fclose(fd);
+    } else {
+        dsme_log(LOG_ERROR, "FAILED enabling thermal sensor %s", CORE_SENSOR_MODE);
+    }
+    return ret;
+}
+
+/* Does our HW support this temp reading ? */
+extern bool dsme_hw_supports_core_temp(void)
+{
+
+    // dsme_log(LOG_DEBUG, "thermal: %s", __FUNCTION__);
+
+    if (enable_hw_core_temp_sensor() &&
+        dsme_hw_get_core_temperature(NULL, NULL)) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+/* Get temp from battery sensor */
+/* TODO: This should be changed to use statefs once we have it */
+
+extern bool dsme_hw_get_battery_temperature(thermal_object_t*         thermal_object,
+                                            temperature_handler_fn_t* callback)
+{
+    int temperature;
+
+    // dsme_log(LOG_DEBUG, "thermal: %s", __FUNCTION__);
+
+    if (read_temperature("/sys/class/power_supply/battery/temp", &temperature)) {
+      /* We get the temp in one tens */
+      temperature = (temperature + 5 ) / 10;
+        if (callback) 
+            callback(thermal_object, temperature);
+        return true;
+    } else {
+        return false;
+    }
+}
+
+/* Does our HW support this temp reading ? */
+extern bool dsme_hw_supports_battery_temp(void)
+{
+
+    if (dsme_hw_get_battery_temperature(NULL, NULL))
+        return true;
+    else
+        return false;
+}
